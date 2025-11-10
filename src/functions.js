7b74426386114d635d81f9e1f0bf35082fc689b5
Overlordka <kopary787@gmail.com>
2025-11-10 11:22:31 +0100

    3 functioner er færdig

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,30 @@
-export function findMaximumNumber(arr) {
-  return Math.max(...arr);
+
+export function processValues(arr) {
+  return arr.map(value => {
+    if(typeof value === "number"){
+        return value * 2
+    } else if (typeof value === "string"){
+        return value.toUpperCase()
+    } else if (value === null || value === undefined){
+        return "N/A"
+    } else {
+        return value
+    }
+  })
+}
+
+export function structureTest(a, b) {
+    return a + b   
+}
+
+export function asyncFunction(success = true) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            if(success) {
+                resolve("success")
+            } else{
+                reject("resjected")
+            }
+        }, 500)
+    })
 }
--- src/functions.test.js
+++ src/functions.test.js
@@ -1,12 +1,45 @@
-import { describe, it, expect } from "vitest";
-import { findMaximumNumber } from "./functions";
+import { describe, it, expect, beforeEach, afterEach } from "vitest"
+import { processValues, structureTest, asyncFunction } from "./functions"
 
-describe("findMaximumNumber", () => {
-  it("should return 5 when finding max in [1, 2, 3, 4, 5]", () => {
-    expect(findMaximumNumber([1, 2, 3, 4, 5])).toBe(5);
-  });
+describe("processValues function", () => {
+  it("skal håndterer både tal, strenge og null-værdier", () => {
+    const example = [1, "hej", null, "verden", 4, undefined, "2"] 
+    expect(processValues(example)).toEqual([2, "HEJ", "N/A", "VERDEN", 8, "N/A", "2"])
+  })
+})
 
-  it("should return 10 when finding max in [-10, -5, 0, 5, 10]", () => {
-    expect(findMaximumNumber([-10, -5, 0, 5, 10])).toBe(10);
-  });
-});
+describe("structureTest function", () => {
+    let numbers
+
+    beforeEach(() => {
+        numbers = {a: 2, b: 4}
+    })
+
+    afterEach(() => {
+        numbers = null
+    })
+
+    it("skal opsummere to tal", () => {
+        expect(structureTest(numbers.a, numbers.b)).toBe(6)
+    })
+
+    it("skal minusere tal", () =>{
+        expect(structureTest(-1, numbers.b)).toBe(3)
+    })
+})
+
+describe("asyncFunction function", () => {
+  it("test af asynkrone funktioner når success er true", async () => {
+    const result = await asyncFunction(true)
+    expect(result).toBe("success")
+  })
+  it("test af asynkrone funktioner når success er false", async () => {
+    try{
+        await asyncFunction(false)
+        throw new Error("det er skåret noget galt")
+    } catch (error){
+        expect(error).toBe("resjected")
+    }
+  })
+})
+
